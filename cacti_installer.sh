26316e3588315201c4d62dfb2fa17872df3ebe86
Mr. Unicorn <48880125+mrun1corn@users.noreply.github.com>
2023-11-18 11:18:00 +0600

    Update cacti_installer.sh

--- cacti_installer.sh
+++ cacti_installer.sh
@@ -59,23 +59,23 @@ install_cacti() {
     # Display welcome message
     echo -e "${GREEN}Welcome to the Cacti Installation Script!${NC}"
 
-# Install Apache & PHP
-if command -v apt &> /dev/null; then
-    # Debian/Ubuntu
-    apt update
-    apt install -yqq apache2 php-mysql libapache2-mod-php php-xml php-ldap php-mbstring php-gd php-gmp php-intl mariadb-server mariadb-client snmp php-snmp rrdtool librrds-perl
-elif command -v yum &> /dev/null; then
-    # Red Hat/CentOS
-    yum makecache
-    yum install -y httpd php php-mysql php-xml php-ldap php-mbstring php-gd php-gmp php-intl mariadb-server mariadb php-snmp rrdtool perl-RRDTool
-else
-    echo -e "${RED}Error: Unsupported package manager.${NC}"
-    exit 1
-fi
+    # Install Apache & PHP
+    if command -v apt &> /dev/null; then
+        # Debian/Ubuntu
+        apt update
+        apt install -yqq apache2 php-mysql libapache2-mod-php php-xml php-ldap php-mbstring php-gd php-gmp php-intl mariadb-server mariadb-client snmp php-snmp rrdtool librrds-perl
+    elif command -v yum &> /dev/null; then
+        # Red Hat/CentOS
+        yum makecache
+        yum install -y httpd php php-mysql php-xml php-ldap php-mbstring php-gd php-gmp php-intl mariadb-server mariadb php-snmp rrdtool perl-RRDTool
+    else
+        echo -e "${RED}Error: Unsupported package manager.${NC}"
+        exit 1
+    fi
 
-# Allow time for package reload to complete
-echo -e "${GREEN}Waiting for package reload to complete.${NC}"
-sleep 5  # Adjust the sleep duration as needed
+    # Allow time for package reload to complete
+    echo -e "${GREEN}Waiting for package reload to complete.${NC}"
+    sleep 5  # Adjust the sleep duration as needed
 
     # Database Tuning
     config_file="/etc/mysql/mariadb.conf.d/50-server.cnf"
@@ -119,54 +119,55 @@ sleep 5  # Adjust the sleep duration as needed
         echo -e "${RED}Error: PHP version not detected or unsupported.${NC}"
     fi
 
-# Create a new site for Cacti
-apache_config_file="/etc/apache2/sites-available/cacti.conf"
-add_lines_to_config "$apache_config_file" "Alias /cacti /opt/cacti"
-add_lines_to_config "$apache_config_file" ""
-add_lines_to_config "$apache_config_file" "  <Directory /opt/cacti>"
-add_lines_to_config "$apache_config_file" "      Options +FollowSymLinks"
-add_lines_to_config "$apache_config_file" "      AllowOverride None"
-add_lines_to_config "$apache_config_file" "      <IfVersion >= 2.3>"
-add_lines_to_config "$apache_config_file" "      Require all granted"
-add_lines_to_config "$apache_config_file" "      </IfVersion>"
-add_lines_to_config "$apache_config_file" "      <IfVersion < 2.3>"
-add_lines_to_config "$apache_config_file" "      Order Allow,Deny"
-add_lines_to_config "$apache_config_file" "      Allow from all"
-add_lines_to_config "$apache_config_file" "      </IfVersion>"
-add_lines_to_config "$apache_config_file" ""
-add_lines_to_config "$apache_config_file" "   AddType application/x-httpd-php .php"
-add_lines_to_config "$apache_config_file" ""
-add_lines_to_config "$apache_config_file" "<IfModule mod_php.c>"
-add_lines_to_config "$apache_config_file" "      php_flag magic_quotes_gpc Off"
-add_lines_to_config "$apache_config_file" "      php_flag short_open_tag On"
-add_lines_to_config "$apache_config_file" "      php_flag register_globals Off"
-add_lines_to_config "$apache_config_file" "      php_flag register_argc_argv On"
-add_lines_to_config "$apache_config_file" "      php_flag track_vars On"
-add_lines_to_config "$apache_config_file" "      # this setting is necessary for some locales"
-add_lines_to_config "$apache_config_file" "      php_value mbstring.func_overload 0"
-add_lines_to_config "$apache_config_file" "      php_value include_path ."
-add_lines_to_config "$apache_config_file" " </IfModule>"
-add_lines_to_config "$apache_config_file" ""
-add_lines_to_config "$apache_config_file" "   DirectoryIndex index.php"
-add_lines_to_config "$apache_config_file" "</Directory>"
+    # Create a new site for Cacti
+    apache_config_file="/etc/apache2/sites-available/cacti.conf"
+    add_lines_to_config "$apache_config_file" "Alias /cacti /opt/cacti"
+    add_lines_to_config "$apache_config_file" ""
+    add_lines_to_config "$apache_config_file" "  <Directory /opt/cacti>"
+    add_lines_to_config "$apache_config_file" "      Options +FollowSymLinks"
+    add_lines_to_config "$apache_config_file" "      AllowOverride None"
+    add_lines_to_config "$apache_config_file" "      <IfVersion >= 2.3>"
+    add_lines_to_config "$apache_config_file" "      Require all granted"
+    add_lines_to_config "$apache_config_file" "      </IfVersion>"
+    add_lines_to_config "$apache_config_file" "      <IfVersion < 2.3>"
+    add_lines_to_config "$apache_config_file" "      Order Allow,Deny"
+    add_lines_to_config "$apache_config_file" "      Allow from all"
+    add_lines_to_config "$apache_config_file" "      </IfVersion>"
+    add_lines_to_config "$apache_config_file" ""
+    add_lines_to_config "$apache_config_file" "   AddType application/x-httpd-php .php"
+    add_lines_to_config "$apache_config_file" ""
+    add_lines_to_config "$apache_config_file" "<IfModule mod_php.c>"
+    add_lines_to_config "$apache_config_file" "      php_flag magic_quotes_gpc Off"
+    add_lines_to_config "$apache_config_file" "      php_flag short_open_tag On"
+    add_lines_to_config "$apache_config_file" "      php_flag register_globals Off"
+    add_lines_to_config "$apache_config_file" "      php_flag register_argc_argv On"
+    add_lines_to_config "$apache_config_file" "      php_flag track_vars On"
+    add_lines_to_config "$apache_config_file" "      # this setting is necessary for some locales"
+    add_lines_to_config "$apache_config_file" "      php_value mbstring.func_overload 0"
+    add_lines_to_config "$apache_config_file" "      php_value include_path ."
+    add_lines_to_config "$apache_config_file" " </IfModule>"
+    add_lines_to_config "$apache_config_file" ""
+    add_lines_to_config "$apache_config_file" "   DirectoryIndex index.php"
+    add_lines_to_config "$apache_config_file" "</Directory>"
 
+    echo "Enter MariaDB root password:"
+    read mariadb_root_password
 
-echo "Enter MariaDB root password:"
-read  mariadb_root_password
-# Create Database and Set Permissions
-sudo mysql -u root -p"${mariadb_root_password}" -e "CREATE DATABASE cacti;"
-sudo mysql -u root -p"${mariadb_root_password}" -e "GRANT ALL ON cacti.* TO cacti@localhost IDENTIFIED BY 'cacti';"
-sudo mysql -u root -p"${mariadb_root_password}" -e "FLUSH PRIVILEGES;"
-# Import MariaDB Time Zone data
-sudo mysql -u root -p"${mariadb_root_password}" mysql < /usr/share/mysql/mysql_test_data_timezone.sql
-# Grant SELECT privilege on time_zone_name table
-sudo mysql -u root -p"${mariadb_root_password}" -e "GRANT SELECT ON mysql.time_zone_name TO cacti@localhost;"
-sudo mysql -u root -p"${mariadb_root_password}" -e "FLUSH PRIVILEGES;"
+    # Create Database and Set Permissions
+    sudo mysql -u root -p"${mariadb_root_password}" -e "CREATE DATABASE cacti;"
+    sudo mysql -u root -p"${mariadb_root_password}" -e "GRANT ALL ON cacti.* TO cacti@localhost IDENTIFIED BY 'cacti';"
+    sudo mysql -u root -p"${mariadb_root_password}" -e "FLUSH PRIVILEGES;"
 
+    # Import MariaDB Time Zone data
+    sudo mysql -u root -p"${mariadb_root_password}" mysql < /usr/share/mysql/mysql_test_data_timezone.sql
 
-	wget https://www.cacti.net/downloads/cacti-latest.tar.gz
-	tar -zxvf cacti-latest.tar.gz
-	sudo mv cacti-1* /opt/cacti
+    # Grant SELECT privilege on time_zone_name table
+    sudo mysql -u root -p"${mariadb_root_password}" -e "GRANT SELECT ON mysql.time_zone_name TO cacti@localhost;"
+    sudo mysql -u root -p"${mariadb_root_password}" -e "FLUSH PRIVILEGES;"
+
+    wget https://www.cacti.net/downloads/cacti-latest.tar.gz
+    tar -zxvf cacti-latest.tar.gz
+    sudo mv cacti-1* /opt/cacti
 
     sudo mysql -u root -p cacti < /opt/cacti/cacti.sql
     config_php_file="/opt/cacti/include/config.php"
