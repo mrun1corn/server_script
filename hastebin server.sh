35d13581005bd2d9104f0a1b9a0b2bf43bbf4896
Mr. Unicorn <48880125+mrun1corn@users.noreply.github.com>
2024-02-07 10:50:01 +0600

    Create hastebin server.sh

---
+++ hastebin server.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt install -y nodejs npm
+git clone --depth 1 https://github.com/seejohnrun/haste-server.git
+cd haste-server
+npm install
+npm start
+
+sudo npm install -g pm2
+pm2 start server.js --name="haste-server"
+pm2 save
+pm2 startup
+pm2 save
